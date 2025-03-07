b5df4d5b8b540e9433da86511121810408cbf45e
alexsfulco3 <alexsfulco3@gmail.com>
2025-03-07 02:55:32 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  float celsius = 30;
+  float fahrenheit = (celsius * 9 / 5) + 32;
+  printf("Celsius: %d, Fahrenheit: %f\n", celsius, fahrenheit);
+  return 0;
+}
