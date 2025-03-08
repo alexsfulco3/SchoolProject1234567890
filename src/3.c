a579c2c2019023e0b8c43708aa33030bfa895654
alexsfulco3 <alexsfulco3@gmail.com>
2025-03-08 07:01:09 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 25;
+    char name[] = "John Doe";
+    float height = 175.0f;
+    printf("Name: %s\nAge: %d\nHeight: %f", name, age, height);
+    return 0;
+}
