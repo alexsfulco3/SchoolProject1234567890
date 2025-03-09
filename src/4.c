f59dac7d3e985788d6fcce24d1fcc3e40ebfd0bb
alexsfulco3 <alexsfulco3@gmail.com>
2025-03-09 23:56:39 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x = 5;
+  if (x == 10) {
+    printf("X is equal to 10.\n");
+  } else {
+    printf("X is not equal to 10.\n");
+  }
+  return 0;
+}
