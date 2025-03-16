33deeda34a1c4f751599f3f227ea71ef9e154c97
alexsfulco3 <alexsfulco3@gmail.com>
2025-03-16 02:49:40 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int age = 25;
+    char name[] = "Alice";
+    float gpa = 3.8;
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("GPA: %.1f\n", gpa);
+
+    return 0;
+}
